2a9b1fc51ff920c2444be0c1f2e9673bec800b37
d0ctr <mr.den.vi@gmail.com>
2023-04-30 22:36:36 +0200

    Init [take 2]

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <blockquote>
+      Tell me and I forget, teach me and I may remember, involve me and I learn.
+    </blockquote>
+  );
+}
+
+export default App;
